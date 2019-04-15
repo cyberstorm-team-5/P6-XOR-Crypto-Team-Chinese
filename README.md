01321745da47496a38ba9e90028856c1272aeb8f
Andrew Maurice <acm1264@yahoo.com>
2019-04-15 09:13:16 -0500

    added initial files
    setup python file and readme, added writeup pdf and the two provided
    tests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# P6-XOR-Crypto-Team-Chinese
-Program 6: XOR Crypto for CSC 442.
+# Program 6: XOR Crypto
+* Authors: Team Chinese (Lane Arnold, Christopher Boquet, Christopher Bouton, Darrell Durousseaux, Clay Fonseca, Rebecca Grantham, Andrew Maurice)
+* Class: Cyber Security 2019 (CSC 442)
+## About
+This repository contains the code and other resources used for the sixth programming assignment, XOR Crypto. The Python code will implement the XOR Crypto method by reading a key from a file named "key" in the current directory, along with the plaintext or ciphertext from stdin, and sending the resulting plaintext or ciphertext to stdout.
---
+++ xor-crypto.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2.7
+################################################################################
+# Authors: Team Chinese (Lane Arnold, Christopher Boquet,
+# 	   Christopher Bouton, Darrell Durousseaux, Clay Fonseca,
+#	   Rebecca Grantham, Andrew Maurice)
+# Class: CSC 442
+# Date: 5-1-2019
+# Github Repo: https://github.com/cyberstorm-team-5/P5-Timelock-Team-Chinese
+# Description: Program 6: Xor Crypto
+#              The Python code will implement the XOR Crypto method by reading
+#              a key from a file named "key" in the current directory, along
+#              with the plaintext or ciphertext from stdin, and sending the
+#              resulting plaintext or ciphertext to stdout.
+################################################################################
+
+#imports here
+
+################################################################################
+
+#global vars here
+DEBUG = True
+
+
+
+################################################################################
+
+#functions here
+
+
+
+###############################MAIN#############################################
+
+
+
+
+
+
+
