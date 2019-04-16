aaaba5c7cfeebe30a036d5e2394448daf171cf72
acm1264 <andromulin@gmail.com>
2019-04-15 19:12:51 -0500

    working implementation of XOR crypto
    tested code with both ciphertexts and their plaintexts

--- xor-crypto.py
+++ xor-crypto.py
@@ -5,7 +5,7 @@
 #	   Rebecca Grantham, Andrew Maurice)
 # Class: CSC 442
 # Date: 5-1-2019
-# Github Repo: https://github.com/cyberstorm-team-5/P5-Timelock-Team-Chinese
+# Github Repo: https://github.com/cyberstorm-team-5/P6-XOR-Crypto-Team-Chinese
 # Description: Program 6: Xor Crypto
 #              The Python code will implement the XOR Crypto method by reading
 #              a key from a file named "key" in the current directory, along
@@ -13,26 +13,28 @@
 #              resulting plaintext or ciphertext to stdout.
 ################################################################################
 
-#imports here
-
-################################################################################
-
-#global vars here
-DEBUG = True
-
-
-
-################################################################################
-
-#functions here
-
-
+import os
+import sys
 
 ###############################MAIN#############################################
 
+#read in plaintext or ciphertext binary file
+m = sys.stdin.read()
 
+#retrieve binary data from the "key" binary file in same directory
+#as this python file
+k = open('key', 'rb').read()
 
+result = ""
 
+#loop through each byte (character) in the message text m and xor with the corresponding
+#index from the key k
+for i in range(len(m)):
+    #key can be shorter than the length of the message, so mod by the length of the key to
+    #loop around as needed
+    result += chr(ord(m[i]) ^ ord(k[i%len(k)]))
 
+#send resulting text to stdout
+print(result)
 
 
